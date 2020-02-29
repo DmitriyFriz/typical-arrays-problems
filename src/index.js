e47a3cfef2512e62998629910c386332647b5672
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2020-02-29 19:11:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && array.length) return Math.min.apply(null, array);
   return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) return Math.max.apply(null, array);
+  return 0;  
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length) {
+    return array.reduce((sum, item) => sum + item , 0) / array.length;
+  }   
   return 0;
 }
